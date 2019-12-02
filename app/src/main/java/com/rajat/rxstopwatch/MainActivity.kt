39317e6c023736d170e82e3cc2aed8aa8a50ce10
Rajat <rajatsinghnegi@hotmail.com>
2019-12-02 16:09:40 +0530

    V2

--- app/src/main/java/com/rajat/rxstopwatch/MainActivity.kt
+++ app/src/main/java/com/rajat/rxstopwatch/MainActivity.kt
@@ -1,60 +1,100 @@
 package com.rajat.rxstopwatch
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.jakewharton.rxbinding3.view.clicks
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.merge
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var compositeDisposable: CompositeDisposable
-    private var ms: String="0"
-    private var hour: Long = 0L
-    private var minutes: Long = 0L
-    private var seconds: Long = 0L
+    private lateinit var timeDisposable:Disposable
+    private var pauseState = BehaviorSubject.create<Long>()
+    private var ms: String = "0"
+    private var hour: String = "0"
+    private var minutes: String = "0"
+    private var seconds: String = "0"
     private var startTime: Long = 0L
+    private lateinit var viewModel:MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         compositeDisposable = CompositeDisposable()
+        viewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
 
-        start.clicks().subscribe {
-            startTime = System.currentTimeMillis()
-            startTimer().subscribe { updateClock(System.currentTimeMillis()) }
-        }
+        listOf<Observable<StopWatchState>>(
+            start.clicks().map { StopWatchState.RUNNING },
+            pause.clicks().map { StopWatchState.PAUSE },
+            reset.clicks().map { StopWatchState.IDLE }).merge()
+            .let { viewModel.registerSubject(it) }
             .let { compositeDisposable.add(it) }
 
-        pause.clicks().subscribe {
-            Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show()
-        }.let { compositeDisposable.add(it) }
 
-        reset.clicks().subscribe {
-            Toast.makeText(this, "reset", Toast.LENGTH_SHORT).show()
-        }.let { compositeDisposable.add(it) }
+        viewModel.uiStates()
+            .subscribe{updateUI(it)}
+            .let { compositeDisposable.add(it) }
+
+//        start.clicks().subscribe {
+//            startTime = System.currentTimeMillis()
+//         timeDisposable =    startTimer().subscribe ({ updateClock(System.currentTimeMillis())},
+//             {},
+//             {Toast.makeText(this,"onComplete",Toast.LENGTH_SHORT).show()})
+//
+//        }.let { compositeDisposable.add(it) }
+//
+//        pause.clicks().subscribe {
+//            Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show()
+//            timeDisposable.dispose()
+//        }.let { compositeDisposable.add(it) }
+//
+//        reset.clicks().subscribe {
+//            Toast.makeText(this, "reset", Toast.LENGTH_SHORT).show()
+//        }.let { compositeDisposable.add(it) }
 
     }
 
-    private fun startTimer(): Observable<Long> {
-        return Observable.interval(0, 1, TimeUnit.MILLISECONDS)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+
+    private fun updateUI(stopWatchTime: StopWatchTime){
+        updateClock(stopWatchTime.time)
+        when (stopWatchTime.state) {
+            StopWatchState.RUNNING -> {
+                start.isEnabled = false
+                pause.isEnabled = true
+                reset.isEnabled = true
+            }
+            StopWatchState.IDLE -> {
+                pause.isEnabled = false
+                reset.isEnabled = false
+                start.isEnabled = true
+            }
+            StopWatchState.PAUSE-> {
+                reset.isEnabled = true
+                start.isEnabled = true
+                pause.isEnabled = false
+            }
+        }
     }
+
 
     private fun updateClock(milliSeconds: Long) {
         val time = (milliSeconds - startTime)
-        ms = String.format("%02d",((time / 10) % 100))
-        seconds = ((time / 1000) % 60)
-        minutes = ((time / 1000) / 60) % 60
-        hour = ((time/1000)/3600)%60
-        clock.text = "$hour:$minutes:$seconds:${ms}"
+        ms = String.format("%02d", ((time / 10) % 100))
+        seconds = String.format("%02d", ((time / 1000) % 60))
+        minutes = String.format("%02d", ((time / 1000) / 60) % 60)
+        hour = String.format("%02d", ((time / 1000) / 3600) % 60)
+        clock.text = "$hour:$minutes:$seconds:$ms"
     }
-
 
     override fun onDestroy() {
         compositeDisposable.dispose()
---
+++ app/src/main/java/com/rajat/rxstopwatch/MyViewModel.kt
@@ -0,0 +1,51 @@
+package com.rajat.rxstopwatch
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
+
+class MyViewModel : ViewModel() {
+
+    var publish = PublishSubject.create<StopWatchState>()
+    private val pause = BehaviorSubject.create<Long>()
+    private val resume = BehaviorSubject.createDefault(0L)
+
+
+    fun registerSubject(observable: Observable<StopWatchState>):Disposable = observable.subscribe {publish.onNext(it)}
+
+    fun uiStates(): Observable<StopWatchTime> =
+        publish.switchMap { it ->
+            when (it) {
+                StopWatchState.IDLE -> {
+                    Observable.just(StopWatchTime())
+                        .doOnNext { pause.onNext(it.time) }
+                        .doOnNext { resume.onNext(it.time) }
+                }
+              StopWatchState.RUNNING -> {
+                    startTimer()
+                        .doOnNext(pause::onNext)
+                        .map { sec -> StopWatchTime(sec, StopWatchState.RUNNING) }
+                }
+                StopWatchState.PAUSE-> {
+                    resume.onNext(pause.value!!)
+                    Observable.just(
+                        StopWatchTime(
+                            pause.value!!,
+                            state = StopWatchState.PAUSE
+                        )
+                    )
+
+                }
+            }
+        }.hide().observeOn(AndroidSchedulers.mainThread())
+
+    private fun startTimer(): Observable<Long> {
+        return Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+            .map {  resume.value!! + it }
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+}
